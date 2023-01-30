185534e4b0b55bd1badb5183a44c0f67eef90c88
tiberedbzt <tibere.dbzt@gmail.com>
2023-01-30 17:28:44 +0100

    Stories : refactor stories

--- stories/button/Button.stories.tsx
+++ stories/button/Button.stories.tsx
@@ -53,155 +53,58 @@ export default {
         color: COLOR_NAMES.PURPLE,
         primary: true,
         flat: false,
-        label: 'Button Label',
+        label: undefined,
         circled: false,
         loading: false,
         disabled: false,
         icon: undefined,
         iconTitle: undefined,
-        onClick: () => console.log('Button Clicked'),
+        onClick: () => {},
     },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
 
 /*
-    * Primary Buttons
+    * Button containing a label
  */
-export const PrimaryLabelPurple = Template.bind({});
-
-PrimaryLabelPurple.args = {
+export const LabelButton = Template.bind({});
+LabelButton.args = {
+    color: COLOR_NAMES.PURPLE,
     label: 'Primary Label Purple',
-    color: COLOR_NAMES.PURPLE,
 }
-
-export const PrimaryIconPurple = Template.bind({});
-
-PrimaryIconPurple.args = {
-    icon: AiOutlinePlus,
-    iconTitle: 'plus icon',
-    color: COLOR_NAMES.PURPLE,
-    circled: true,
-}
-
-export const PrimaryIconWhite = Template.bind({});
-
-PrimaryIconWhite.args = {
-    icon: AiOutlinePlus,
-    iconTitle: 'plus icon',
-    color: COLOR_NAMES.WHITE,
-    circled: true,
-}
-
-export const PrimaryLabelWhite = Template.bind({});
-
-PrimaryLabelWhite.args = {
-    label: 'Primary Label White',
-    color: COLOR_NAMES.WHITE,
-}
-
-PrimaryLabelWhite.parameters = {
-    backgrounds: {
-        default: COLOR_NAMES.PURPLE,
-    }
-}
-
 
 /*
-    * Secondary Buttons
+    * Button containing an icon
  */
-export const SecondaryLabelPurple = Template.bind({});
-
-SecondaryLabelPurple.args = {
-    primary: false,
-    label: 'Secondary Label Purple',
+export const IconButton = Template.bind({});
+IconButton.args = {
     color: COLOR_NAMES.PURPLE,
-}
-
-export const SecondaryLabelWhite = Template.bind({});
-
-SecondaryLabelWhite.args = {
-    primary: false,
-    label: 'Secondary Label White',
-    color: COLOR_NAMES.WHITE,
-}
-
-SecondaryLabelWhite.parameters = {
-    backgrounds: {
-        default: COLOR_NAMES.PURPLE,
-    }
-}
-
-export const SecondaryLabelBlack = Template.bind({});
-
-SecondaryLabelBlack.args = {
-    label: 'Primary Label Black',
-    color: COLOR_NAMES.BLACK,
-}
-
-
-/*
-    * Flat Buttons
- */
-
-export const FlatLabelPurple = Template.bind({});
-
-FlatLabelPurple.args = {
-    primary: false,
-    label: 'Flat Label Purple',
-    color: COLOR_NAMES.PURPLE,
-    flat: true,
-}
-
-export const FlatLabelWhite = Template.bind({});
-
-FlatLabelWhite.args = {
-    primary: false,
-    label: 'Flat Label White',
-    color: COLOR_NAMES.WHITE,
-    flat: true,
-}
-
-FlatLabelWhite.parameters = {
-    backgrounds: {
-        default: COLOR_NAMES.PURPLE,
-    }
-}
-
-export const FlatIconWhite = Template.bind({});
-
-FlatIconWhite.args = {
-    primary: false,
     icon: AiOutlinePlus,
     iconTitle: 'plus icon',
-    color: COLOR_NAMES.WHITE,
-    flat: true,
 }
 
-FlatIconWhite.parameters = {
-    backgrounds: {
-        default: COLOR_NAMES.PURPLE,
-    }
-}
-
-export const PrimaryLabelIconPurple = Template.bind({});
-
-PrimaryLabelIconPurple.args = {
-    primary: true,
+/*
+    * Button containing a label and an icon
+ */
+export const LabelIconButton = Template.bind({});
+LabelIconButton.args = {
     color: COLOR_NAMES.PURPLE,
     label: 'Primary Label Icon Purple',
     icon: AiOutlinePlus,
     iconTitle: 'plus icon',
 }
 
-export const ButtonAction = Template.bind({});
-
-ButtonAction.args = {
+/*
+    * Button containing a label and responding to a click callback
+ */
+export const ClickLabelButton = Template.bind({});
+ClickLabelButton.args = {
     color: COLOR_NAMES.PURPLE,
     label: 'Button Action',
+    onClick: () => console.log('Button clicked'),
 }
-
-ButtonAction.play = async ({canvasElement}) => {
+ClickLabelButton.play = async ({canvasElement}) => {
     const canvas = within(canvasElement);
     fireEvent.click(canvas.getByRole('button', {name: 'Button Action'}));
 }
